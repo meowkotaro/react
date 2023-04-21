e9e117ae32d542748a16f11a2a8c1a1b9a4f7633
hashimoto <meowkotaro3109@gmail.com>
2023-04-21 23:56:41 +0900

    footerの追加、ナビメニュー用のコンテキストフォルダを追加

--- react/pages/index.js
+++ react/pages/index.js
@@ -1,15 +1,15 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import HeaderLayout from "../components/headerLayout"
+import FooterLayout from '@/components/footerLayout'
+import { NavMenuProvider } from '@/context/navMenuProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      <HeaderLayout>
-        
-      </HeaderLayout>
-    </>
+    <NavMenuProvider>
+      <HeaderLayout/>
+      <FooterLayout/>
+    </NavMenuProvider>
   )
 }
